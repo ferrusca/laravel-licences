d38abef0b3946d7194f09b776e273b9d497022fe
JorgeLuis <estebanracing21@gmail.com>
2020-06-13 19:11:51 -0500

    Create migrations

---
+++ database/migrations/2020_06_13_231101_create_citas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('rfc');
+            $table->integer('modulo_atencion_id')->unsigned();
+            $table->foreign('modulo_atencion_id')->references('id')->on('modulos_atencion');
+            $table->timestamps();
+            $table->integer('tramite_id')->unsigned();
+            $table->foreign('tramite_id')->references('id')->on('tramites');
+            $table->dateTime('horario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+}
--- database/migrations/2020_06_13_235831_create_horarios_table.php
+++ database/migrations/2020_06_13_235831_create_horarios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateHorariosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateFailedJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('horarios', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('tipo_dia');
+            $table->integer('hora_inicio')->unsigned();
+            $table->integer('hora_fin')->unsigned();
+            $table->string('descripcion');
+            $table->timestamps();
         });
     }
 
@@ -30,6 +30,6 @@ class CreateFailedJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('horarios');
     }
 }
