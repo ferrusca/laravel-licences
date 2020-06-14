295231f09bc5ad78a3e8b57145a8fa380593a82d
JorgeLuis <estebanracing21@gmail.com>
2020-06-13 19:12:06 -0500

    Create seeds for tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\ModuloAtencion;
+use App\Tramite;
+use App\Horario;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->delete();
+        DB::table('password_resets')->delete();
+        ModuloAtencion::create([
+            'Nombre' => 'Oficina Central Insurgentes',
+            'Direccion' => 'Insurgentes 860, Napoles. CDMX'
+        ]);
+        ModuloAtencion::create([
+            'Nombre' => 'Módulo Móvil 30',
+            'Direccion' => 'Av. Universidad 3000, Ciudad Universitaria. CDMX'
+        ]);
+        ModuloAtencion::create([
+            'Nombre' => 'Módulo Móvil 18',
+            'Direccion' => 'Av. Canal de Tezontle 118, Alfonso Ortiz Tirado. CDMX'
+        ]);
+        Tramite::create([
+            'Nombre' => 'Reposición de Tarjeta de Circulación',
+            'Prioridad' => 1
+        ]);
+        Tramite::create([
+            'Nombre' => 'Renovación de Tarjeta de Circulación',
+            'Prioridad' => 2
+        ]);
+        Horario::create([
+            'tipo_dia' => 'habil',
+            'hora_inicio' => 540,
+            'hora_fin' => 900,
+            'descripcion' => 'Horario de servicio en dias habiles'
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
