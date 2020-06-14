3e10dcbf90cf3a0011c776274c6de7a4074bc68d
JorgeLuis <estebanracing21@gmail.com>
2020-06-14 14:32:10 -0500

    Rename field RFC to CURP

--- app/Cita.php
+++ app/Cita.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cita extends Model
 {
     protected $fillable = [
-        'rfc', 'modulo_atencion_id', 'tramite_id', 'horario'
+        'curp', 'modulo_atencion_id', 'tramite_id', 'horario'
     ];
     
     public function moduloAtencion() {
